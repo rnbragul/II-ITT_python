49bc36375f557c1e154897f68e624046e97df0d1
Ragul <rnbragul@gmail.com>
2026-04-10 11:22:17 +0530

    Create basics.py

---
+++ ex1/basics.py
@@ -0,0 +1,20 @@
+print(10//3)
+print(bool(0))
+print(type([]))
+a="HELLO"
+a.lower()
+print(a)
+for i in range (3):
+   print(i)
+print(range(3))
+print("PYTHON"[0])
+print(232323)
+
+
+for i in range(0,10):
+      print(i)
+
+print(bool(0))
+print (float(bool(int(0))))
+a=10.00
+b=int(a)
